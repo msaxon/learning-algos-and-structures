29a09429ee2d60d866b20cee7406b74a65768228
Matthew Saxon <maestromattaeus@gmail.com>
2020-09-01 20:36:36 -0500

    adding some comments

--- src/main/scala/com/maestro/learn/algorithms/sorting/InsertionSorter.scala
+++ src/main/scala/com/maestro/learn/algorithms/sorting/InsertionSorter.scala
@@ -1,5 +1,14 @@
 package com.maestro.learn.algorithms.sorting
 
+/**
+  * Time Complexity
+  *   Best Case: O(n)
+  *   Average Case: O(n^2)
+  *   Worst Case: O(n^2)
+  *
+  * Space Complexity
+  *   Worst Case: O(1)
+  */
 object InsertionSorter {
   def sort(arr: Array[Int]): Array[Int] = {
     for(i <- 1 until arr.length) {
--- src/main/scala/com/maestro/learn/algorithms/sorting/SelectionSorter.scala
+++ src/main/scala/com/maestro/learn/algorithms/sorting/SelectionSorter.scala
@@ -1,5 +1,14 @@
 package com.maestro.learn.algorithms.sorting
 
+/**
+  * Time Complexity
+  *   Best Case: O(n^2)
+  *   Average Case: O(n^2)
+  *   Worst Case: O(n^2)
+  *
+  * Space Complexity
+  *   Worst Case: O(1)
+  */
 object SelectionSorter {
   def sort(arr: Array[Int]):Array[Int] = {
     for(i <- 0 until arr.length) {
